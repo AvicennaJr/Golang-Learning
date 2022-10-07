fc2bd17326e0c4b70eb1f3e30e82774078532c7a
Lugano Abel <lugano.ngulwa@gmail.com>
2022-10-07 22:02:44 +0300

    Create main.go

---
+++ Hopertz/unit1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+   fmt.Println("Hello, world!")
+  
+   displayTime() //Because the displayTime() function belongs to the main package, it can also be called from main.go
+
+}
