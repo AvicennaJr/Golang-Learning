35e746bb2b9424bcd1e5de4169cf17b848dfa789
Lugano Abel <lugano.ngulwa@gmail.com>
2022-10-23 23:56:33 +0300

    Create encodingInterfacesJson.go

---
+++ Hopertz/unit10/encodingInterfacesJson.go
@@ -0,0 +1,35 @@
+package main
+import (
+ "encoding/json"
+ "fmt"
+ "time"
+)
+type Customer map[string]interface{}
+type Name map[string]interface{}
+type Address map[string]interface{}
+
+  func main() {
+	 layoutISO := "2006-01-02"
+	 dob, _ := time.Parse(layoutISO, "2010-01-18")
+	 john := Customer{
+	     "Name": Name{ 
+			"FirstName": "John",
+	        "LastName": "Smith",
+	     },
+	     "Email": "johnsmith@example.com",
+	     "Address": Address{
+	        "Line1": "The White House",
+	        "Line2": "1600 Pennsylvania Avenue NW",
+	        "Line3": "Washington, DC 20500",
+	     },
+	     "DOB": dob,
+	}
+	johnJSON, err := json.MarshalIndent(john, "", " ")
+	if err == nil {
+ 	    fmt.Println(string(johnJSON))
+	} else {
+    	fmt.Println(err)
+	}
+
+	// The order of the keys in the output JSON is sorted alphabetically.
+   }
