42963cb0144ce4b688fc6e35e3bc1bb50897c9d5
Lugano Abel <lugano.ngulwa@gmail.com>
2022-10-23 23:53:25 +0300

    Create mapUnstructuredData.go

---
+++ Hopertz/unit9/mapUnstructuredData.go
@@ -0,0 +1,33 @@
+package main
+import (
+ "encoding/json"
+ "fmt"
+)
+func main() {
+      jsonString :=
+      `{
+           "success": true,
+           "timestamp": 1588779306,
+           "base": "EUR",
+           "date": "2020-05-06",
+           "rates": {
+                  "AUD": 1.683349,
+                  "CAD": 1.528643,
+                  "GBP": 0.874757,
+                  "SGD": 1.534513,
+                  "USD": 1.080054
+            }
+        }`
+ var result map[string]interface{}
+ json.Unmarshal([]byte(jsonString), &result)
+ fmt.Println(result["success"]) // true
+ rates := result["rates"]
+ fmt.Println(rates)
+ // map[USD:1.080054 AUD:1.683349 CAD:1.528643
+ // GBP:0.874757 SGD:1.534513]
+
+ currencies := rates.(map[string]interface{})
+ SGD := currencies["SGD"]
+ fmt.Println(SGD)
+ // 1.534513
+}
