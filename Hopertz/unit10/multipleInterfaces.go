fe223ef551cc1a5cc2e2b9092e51881f45edf883
Lugano Abel <lugano.ngulwa@gmail.com>
2022-10-24 15:53:12 +0300

    Create multipleInterfaces.go

---
+++ Hopertz/unit10/multipleInterfaces.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	 "math"
+)
+
+type Shape interface {
+ Area() float64
+}
+type Circle struct {
+ radius float64
+}
+
+   
+// Circle implements Shape
+func (c Circle) Area() float64 {
+	return 3.14 * c.radius * c.radius
+}
+
+func (c Circle) Circumference() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+
+func (c Circle) String() string {
+	return fmt.Sprintf("Area is %v Circumference is %v",c.Area(), c.Circumference())
+   
+}
+
+
+func main() {
+
+	c1 := Circle{radius: 5}
+	fmt.Println(c1)
+
+
+	
+}
