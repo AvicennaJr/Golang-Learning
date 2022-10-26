037d35f11daf2a2c3d8f9d8ad337ceaa7544cf51
Lugano Abel <lugano.ngulwa@gmail.com>
2022-10-26 23:05:39 +0300

    Create usecaseChannels.go

---
+++ Hopertz/unit12/usecaseChannels.go
@@ -0,0 +1,40 @@
+package main 
+
+import (
+	"fmt"
+	"rand"
+)
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+	    sum += v
+	}
+	c <- sum
+}
+func main() {
+	s := []int{}
+	sliceSize := 10
+	for i := 0; i < sliceSize; i++ {
+	    s = append(s, rand.Intn(100))
+	}
+	c := make(chan int)
+	partSize := 2
+	parts := sliceSize / partSize
+	i := 0
+	for i < parts {
+	     go sum(s[i*partSize:(i+1)*partSize], c)
+	     i += 1
+	}
+	i = 0
+	total := 0
+	for i < parts {
+	    partialSum := <-c // read from channel
+	    fmt.Println("Partial Sum: ", partialSum)
+	    total += partialSum
+	    i += 1
+	}
+	fmt.Println("Total: ", total)
+	fmt.Scanln()
+   }
+
